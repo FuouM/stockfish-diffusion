a671ae6120eb2fc6d181dea6426ee33f51da142c
FuouM <wr0n9.nuw133r@gmail.com>
2023-02-19 22:56:15 +0700

    Add concat

---
+++ util_stuff/concat.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+from torch.utils.data import ConcatDataset as _ConcatDataset
+
+from audio_stuff.audio_folder import DATASETS
+
+
+@DATASETS.register_module()
+class ConcatDataset(_ConcatDataset):
+    def __init__(self, datasets: Iterable[dict], collate_fn=None) -> None:
+        """Concatenate multiple datasets.
+
+        Args:
+            datasets (Iterable[dict]): Datasets to concatenate.
+            collate_fn (Callable, optional): Collate function. Defaults to None.
+        """
+
+        super().__init__([DATASETS.build(dataset) for dataset in datasets])
+
+        self.collate_fn = collate_fn
